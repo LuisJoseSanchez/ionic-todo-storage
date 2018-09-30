c2151658a62199a6fce4f3ed4b621655653dd255
Luis Jose Sanchez <luisjoseprofe@gmail.com>
2018-09-30 13:52:24 +0200

    Fixing todos with localstorage.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TodoService } from '../services/todo.service';
 import { Todo } from '../interfaces/todo';
+import { AlertController, NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -11,14 +12,47 @@ export class HomePage implements OnInit {
 
   todos: Todo[] = [];
 
-  constructor(private todoService: TodoService) {
-    
-  }
+  constructor(
+    private todoService: TodoService,
+    private navControler: NavController,
+    private alertController: AlertController) { }
 
   ngOnInit() {
-    this.todoService.load().then(
-      () => this.todos = this.todoService.getTodos()
+    this.todoService.getTodos().then(
+      data => this.todos = data
     );
-    
   }
+
+  goEditTodo(id: number) {
+    this.navControler.navigateForward('/edit/' + id);
+  }
+
+  async deleteDialog(id: number, title: string) {
+
+      const alert = await this.alertController.create({
+        header: 'Borrar tarea',
+        message: '¿Estás seguro que quieres borrar la tarea <b>"' + title + '"</b>?',
+        buttons: [
+          {
+            text: 'Cancelar',
+            role: 'cancel',
+            cssClass: 'secondary',
+            /*handler: (blah) => {
+              console.log('Confirm Cancel: blah');
+            }*/
+          }, {
+            text: 'Aceptar',
+            handler: () => {
+              this.todoService.deleteTodo(id);
+              this.todoService.getTodos().then(
+                data => this.todos = data
+              );
+            }
+          }
+        ]
+      });
+  
+      await alert.present();
+  }
+
 }
--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -8,9 +8,24 @@ import { Storage } from '@ionic/storage';
 export class TodoService {
 
   todos: Todo[] = [];
+  todosCounter = 0;
 
-  constructor(private storage: Storage) { }
+  constructor(private storage: Storage) {
+    this.getTodos().then(
+      data => {
+        this.todos = data;
+        this.todos.forEach(
+          t => {
+            if (t.id > this.todosCounter) {
+              this.todosCounter++;
+            }
+          });
+        this.todosCounter++;
+      }
+    );
+  }
 
+  /*
   load(): Promise<Boolean> {
 
     return new Promise(
@@ -27,28 +42,53 @@ export class TodoService {
       }
     );
   }
+*/
 
-  save(t: Todo) {
-    console.log('Saving...');
-    console.log(t);
-    this.todos[t.id] = t;
-    this.storage.set('todos', this.todos);
-    console.log(this.todos);
-  }
-
-  getTodos(): Todo[] {
-    return this.todos;
+  /*
+    save(t: Todo) {
+      console.log('Saving...');
+      console.log(t);
+      this.todos[t.id] = t;
+      this.storage.set('todos', this.todos);
+      console.log(this.todos);
+    }
+  */
+  getTodos(): Promise<Todo[]> {
+    return this.storage.get('todos');
   }
 
   getTodo(id: number): Todo {
     return this.todos.find(t => t.id == id);
   }
 
+  /*
   addTodo(t: Todo) {
     console.log('Adding todo...');
     console.log(t);
     this.todos.push(t);
     console.log(this.todos);
+    this.storage.set('todos', this.todos);
+  }
+*/
+
+  saveTodo(t: any) {
+    if (t.id) {
+      let index = this.todos.findIndex(todo => todo.id === t.id);
+      this.todos[index] = {
+        id: t.id,
+        title: t.title,
+        description: t.description
+      }
+    } else {
+      t.id = this.todosCounter;
+      this.todos.push(t);
+      this.todosCounter++;
+    }
+    this.storage.set('todos', this.todos);
+  }
+
+  deleteTodo(id: number) {
+    this.todos = this.todos.filter(t => t.id != id);
     this.storage.set('todos', this.todos);
   }
 }
